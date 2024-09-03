61d2012c149a5fae0116d2e84689c0849d63ded3
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-09-03 02:04:42 -0400

    Updates

---
+++ precache-manifest.561d7bf6c391f40084c0998675d22f1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdb6c64e6121e3dbf65c088cf0df8bc",
+    "url": "/kawthar-mashkour/index.html"
+  },
+  {
+    "revision": "0489dde7757212cb43c4",
+    "url": "/kawthar-mashkour/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "4ede2c8c6f7b7617e61d",
+    "url": "/kawthar-mashkour/static/css/main.9ac7d567.chunk.css"
+  },
+  {
+    "revision": "0489dde7757212cb43c4",
+    "url": "/kawthar-mashkour/static/js/2.ad7bc7bd.chunk.js"
+  },
+  {
+    "revision": "ebdb640f0f0788ae6d1c10dbdb1f380f",
+    "url": "/kawthar-mashkour/static/js/2.ad7bc7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27204ce6dab193db3936",
+    "url": "/kawthar-mashkour/static/js/3.1ad7a169.chunk.js"
+  },
+  {
+    "revision": "4ede2c8c6f7b7617e61d",
+    "url": "/kawthar-mashkour/static/js/main.5028610d.chunk.js"
+  },
+  {
+    "revision": "6c8fe8c4d0b646d9204e",
+    "url": "/kawthar-mashkour/static/js/runtime-main.6ebbb8de.js"
+  }
+]);
